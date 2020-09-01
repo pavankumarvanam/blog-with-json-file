a4a8ccd26103b86234bfd3df912201d424f58f91
pavankumarvanam <v.pavan@ailabs.in>
2020-09-01 19:29:43 +0530

    Updates

---
+++ precache-manifest.da1134e398d5171e13539ef689336bdc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74c53404fde4a0ce2caefc7cdf2e592",
+    "url": "/blog-with-json-file/index.html"
+  },
+  {
+    "revision": "c0b15932c8a690c490ba",
+    "url": "/blog-with-json-file/static/css/main.141b4e72.chunk.css"
+  },
+  {
+    "revision": "0c111708c79d3704d6b9",
+    "url": "/blog-with-json-file/static/js/2.9eaed6d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-with-json-file/static/js/2.9eaed6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b15932c8a690c490ba",
+    "url": "/blog-with-json-file/static/js/main.73112160.chunk.js"
+  },
+  {
+    "revision": "3b994bf747e115b6981a",
+    "url": "/blog-with-json-file/static/js/runtime-main.819d385e.js"
+  },
+  {
+    "revision": "dbd2ac93e2df6492fe3ad4b4b27f368c",
+    "url": "/blog-with-json-file/static/media/blog_image_1.dbd2ac93.jpeg"
+  },
+  {
+    "revision": "8c96f85fb7023d38002f0666b7ce815b",
+    "url": "/blog-with-json-file/static/media/blog_image_2.8c96f85f.jpg"
+  },
+  {
+    "revision": "5cf26f68891c31ddcbdb51ed7656703d",
+    "url": "/blog-with-json-file/static/media/blog_image_3.5cf26f68.jpeg"
+  },
+  {
+    "revision": "d2044fe2954ea7672bc03f6937150c96",
+    "url": "/blog-with-json-file/static/media/profile.d2044fe2.jpeg"
+  },
+  {
+    "revision": "5ef8eaec8598aa4be986424a31465abb",
+    "url": "/blog-with-json-file/static/media/two.5ef8eaec.jpeg"
+  }
+]);
